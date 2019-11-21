2a1c4dece5cb41f1b761e7267f65559b96e9d188
b1ueb1ues <bblues@live.cn>
2019-11-21 17:44:57 +0800

    update

--- frida/__main__.py
+++ frida/__main__.py
@@ -10,6 +10,8 @@ def main(mod, prepare):
         mod = sys.argv[1]
     if len(sys.argv) > 2:
         prepare = True
+    if '.js' not in mod:
+        mod += '.js'
     zaga.run('%s'%mod, None, prepare)
 
 main(default, prepare)
