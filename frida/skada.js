705300e33c8022c78583f5653bf205118f55733a
b1ueb1ues <bblues@live.cn>
2019-12-18 17:57:23 +0800

    fix dot recount bug

--- frida/skada.js
+++ frida/skada.js
@@ -97,6 +97,9 @@ function recount(type, dmg, iscrit, cha, src, dst){
         var aid = -1;
         var idx = -1;
     }
+    if (type == 'cb::apsd'){
+        aid = -1;
+    }
     //var from = '<'+ci+'>'+' ,[,'+ ct+','+dpi+','+dpp+','+aid+','+idx   +',]' ;
     var from = ci+' ,[,'+ ct+','+dpi+','+dpp+','+aid+','+idx   +',]' ;
 
