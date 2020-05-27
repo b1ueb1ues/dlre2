3fc9e84712cf33fa9b85fc4a22488a15992d719b
b1ueb1ues <bblues@live.cn>
2020-05-27 10:15:42 +0800

    1.20.0

--- bcr/qs.py
+++ bcr/qs.py
@@ -2,55 +2,136 @@ class unit() :
     def __init__(this, i, v):
         this.i = i
         this.v = v
+        this.debug = 0
+
     def __repr__(this):
-        return str(this.i)
+        if this.debug:
+            return str("%s(%s)"%(this.i, this.v))
+        else:
+            return str(this.i)
 
-def qs(a, lo, hi):
-    if lo < hi :
-        p = part(a, lo, hi)
-        qs(a, lo, p-1)
-        qs(a, p+1, hi)
+class sort() :
+    def __init__(this, a):
+        this.a = a
 
-def part(a, lo, hi):
-    pivot = a[hi]
-    i = lo
-    for j in range(lo, hi):
-        if a[j].v < pivot.v:
-            a[j],a[i] = a[i],a[j]
+    def compare(this, x, y):
+        if this.a[x].v > this.a[y].v:
+            return 1
+        if this.a[x].v == this.a[y].v:
+            return 0
+        if this.a[x].v < this.a[y].v:
+            return -1
+
+    def compare_r(this, x, y):
+        if this.a[x].v < this.a[y].v:
+            return 1
+        if this.a[x].v == this.a[y].v:
+            return 0
+        if this.a[x].v > this.a[y].v:
+            return -1
+
+    def swap(this, x, y):
+        this.a[x], this.a[y] = this.a[y], this.a[x]
+
+    def sort(this, a, lo, hi):
+        pass
+
+    def __call__(this):
+        this.sort(this.a, 0, len(this.a)-1)
+
+class qs(sort):
+    def sort(this, a, lo, hi):
+        if lo < hi:
+            pivot_index = this.partition(a, lo, hi)
+            this.sort(a, lo, pivot_index)
+            this.sort(a, pivot_index+1, hi)
+
+    def partition(this, a, lo, hi):
+        pivot = int((hi+lo)/2)
+        i = lo
+        j = hi
+        while 1:
+            while this.compare(i, pivot) < 0:
+                i += 1
+            while this.compare(j, pivot) > 0:
+                j -= 1
+            if i >= j :
+                return j
+            this.swap(i, j)
             i += 1
-    a[i],a[hi] = a[hi],a[i]
-    return i
+            j -= 1
 
 
-def main(c): 
+def main(F, t): 
     A = []
-    idx = 1
-    for i in c:
+    idx = len(t)
+    for i in t:
         A.append(unit(idx, i))
-        idx += 1
+        idx -= 1
 
     a = A[:5]
-    qs(a,0,4)
+    f = F(a)
+    f()
     print(a)
 
 if __name__ == '__main__':
-    c = [0,0,0,0,0]
-    main(c)
 
-    c = [0,1,1,1,1]
-    main(c)
+    t = [5,4,3,2,1]
+    main(qs, t)
+    t = [1,2,3,4,5]
+    main(qs, t)
 
-    c = [1,0,1,1,1]
-    main(c)
+    print('===========')
 
-    c = [1,1,0,1,1]
-    main(c)
+    t = [1,1,1,1,1]
+    main(qs, t)
+    t = [1,1,1,1]
+    main(qs, t)
+    t = [1,1,1]
+    main(qs, t)
+    t = [1,1]
+    main(qs, t)
+    t = [1]
+    main(qs, t)
 
-    c = [1,1,1,0,1]
-    main(c)
+    print('5-----------')
+    t = [1,1,1,1,1]
+    main(qs, t)
+    t = [1,0,1,1,1]
+    main(qs, t)
+    t = [1,1,0,1,1]
+    main(qs, t)
+    t = [0,1,1,1,1]
+    main(qs, t)
 
-    c = [1,1,1,1,0]
-    main(c)
+    print('4-----------')
+    t = [1,1,1,1]
+    main(qs, t)
+    t = [1,0,1,1]
+    main(qs, t)
+    t = [0,1,1,1]
+    main(qs, t)
 
-    c = [1,0,1,0,1]
-    main(c)
+    print('3-----------')
+    t = [1,1,1]
+    main(qs, t)
+    t = [0,1,1]
+    main(qs, t)
+    t = [1,0,1]
+    main(qs, t)
+
+    print('2-----------')
+    t = [1,1]
+    main(qs, t)
+
+    print('t-----------')
+    t = [1,1,1,1,0]
+    main(qs, t)
+    t = [1,1,1,0]
+    main(qs, t)
+    t = [1,1,0]
+    main(qs, t)
+
+    print('p-----------')
+    t = [3,3,2,2,1]
+    main(qs, t)
